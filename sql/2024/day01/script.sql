d6f529494f6cdeb7691b8d141ce715694bb156d2
YourAverageMoron <ryannffc21@gmail.com>
2024-12-07 18:08:31 +0000

    2024 day01 question 2 sql

--- sql/2024/day01/script.sql
+++ sql/2024/day01/script.sql
@@ -16,4 +16,21 @@ FROM INPUT)
 insert into output (question, answer) values (
     'Question 1',
     (select sum(abs(a.value - b.value)) from a join b on a.id = b.id)
-)
+);
+
+------------------
+
+with a as (SELECT
+    cast(SUBSTR(line, 1, INSTR(line, '   ') - 1) as INTEGER) AS value
+FROM INPUT),
+
+b as (SELECT
+    cast(SUBSTR(line, INSTR(line, '   ') + 1) as INTEGER) AS value
+FROM INPUT),
+
+c as (select value, count(value) c from b group by value)
+
+insert into output (question, answer) values (
+    'Question 2',
+    (select sum(a.value * c.c) from a join c on a.value = c.value)
+);
