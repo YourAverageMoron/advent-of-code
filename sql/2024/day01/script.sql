95f1b929640901156c366d5edde0443b03fb006a
YourAverageMoron <ryannffc21@gmail.com>
2024-12-07 15:51:31 +0000

    2024 Day1 question1 in sql

--- sql/2024/day01/script.sql
+++ sql/2024/day01/script.sql
@@ -1,1 +1,19 @@
-create table output as select * from input;
+with a as (SELECT
+    ROW_NUMBER () OVER ( 
+        ORDER BY cast(SUBSTR(line, 1, INSTR(line, '   ') - 1) as INTEGER)
+    ) id,
+    cast(SUBSTR(line, 1, INSTR(line, '   ') - 1) as INTEGER) AS value
+FROM INPUT),
+
+b as (SELECT
+    ROW_NUMBER () OVER ( 
+        ORDER BY  cast(SUBSTR(line, INSTR(line, '   ') + 1) as INTEGER)
+    ) id,
+    cast(SUBSTR(line, INSTR(line, '   ') + 1) as INTEGER) AS value
+FROM INPUT)
+
+
+insert into output (question, answer) values (
+    'Question 1',
+    (select sum(abs(a.value - b.value)) from a join b on a.id = b.id)
+)
--- sql/lib/run.sh
+++ sql/lib/run.sh
@@ -1,9 +1,16 @@
 YEAR=$1
 DAY=$2
 
+DATA_FILE_NAME="input.txt"
+
+if [ $DEMO ]; then
+    DATA_FILE_NAME="input_demo.txt"
+fi
+
 sqlite3 "" \
     "create table input(line TEXT);" \
-    ".import ./data/$YEAR/$DAY/input.txt input" \
+    ".import ./data/$YEAR/$DAY/$DATA_FILE_NAME input" \
+    "create table output (question text, answer text);" \
     ".read ./sql/$YEAR/$DAY/script.sql" \
     "select * from output;" \
     ".exit"
